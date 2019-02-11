d7a05352a1fe010f8e65be3376d6229d81c0a9a4
Ali Sheikh <ali.sheikh@gmx.at>
2019-02-11 12:25:40 +0100

    implemented MD5HelperNTest implemented RijndaelManagerNTest
    Signed-off-by: Ali Sheikh <ali.sheikh@gmx.at>

---
+++ NUnitTest.Olymp.Util/MD5HelperNTest.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using Olymp.Util;
+
+namespace NUnitTest.Olymp.Util
+{
+    [TestFixture]
+    public class MD5HelperNTest
+    {
+        [Test]
+        public void EnsureMD5CalculationWorksProperly()
+        {
+            const string HashToCheck = "428F28BDFD1051F3CEAAD7E16C9F7E9C";
+            const string PasswordToHash = "VeryS3cur3P0ssw0rd";
+
+            var calulatedHash = MD5Helper.CalculateMD5Hash(PasswordToHash);
+            Assert.That(calulatedHash.Equals(HashToCheck));
+        }
+    }
+}
---
+++ NUnitTest.Olymp.Util/RijndaelManagerNTest.cs
@@ -0,0 +1,22 @@
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Olymp.Util;
+
+namespace NUnitTest.Olymp.Util
+{
+    [TestFixture]
+    public class RijndaelManagerNTest
+    {
+        [Test]
+        public void EnsureWorksRijndaelManagerProperly()
+        {
+            const string TestPassword = "VeryS3cur3P0ssw0rd";
+            var TextToCheck = Encoding.ASCII.GetBytes("this text is a test.");
+
+            var calculedByteArry = RijndaelManager.Encrypt(TextToCheck, TestPassword);
+
+            Assert.That(RijndaelManager.Decrypt(calculedByteArry, TestPassword).SequenceEqual(TextToCheck));
+        }
+    }
+}
